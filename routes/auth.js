0eda7a26491dd51f6ad04a2bd59a07b91eff8ab4
GUNASAIKUMAR3 <gunasaikumar3git@gmail.com>
2025-02-09 16:48:25 +0530

    commit

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const { registerUser, loginUser } = require("../controllers/authController.js");
+
+// Sign-up route
+router.post("/signup", registerUser);
+
+// Login route
+router.post("/login", loginUser);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const dotenv = require("dotenv");
+const app = express();
+const port = process.env.PORT || 5050;
+dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL;
+app.use(cors());
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log("Error connecting to DB:", err));
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
